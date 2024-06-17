8370fe884c4d896330831f8983fd5d099ae7dc86
tommy-pellerin <tommytp@hotmail.fr>
2024-06-17 14:09:36 +0200

    welcome mail ok on dev

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: ENV["MAILER_SENDER"]
+  default from: ENV['MAILJET_NOREPLY_FROM']
 
   def welcome_email(user)
     @user = user
-    @url  = "http://example.com/login"
-    mail(to: @user.email, subject: "Welcome to My Awesome Site")
+    @url  = "http://keella.fly.io/login"
+    mail(to: @user.email, subject: "Bienvenue sur notre site")
   end
 end
