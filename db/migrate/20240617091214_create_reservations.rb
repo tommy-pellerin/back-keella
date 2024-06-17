3f09850980bd1c20e484e270d0e0caec40d212fe
WinnyDrancourt <winny.morel@orange.fr>
2024-06-17 11:14:21 +0200

    Migration Workouts

---
+++ db/migrate/20240617091214_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[7.2]
+  def change
+    create_table :reservations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :workout, null: false, foreign_key: true
+      t.integer :quantity
+      t.float :total
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
