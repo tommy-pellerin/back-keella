a639e708a7982a20722949a5f6462eb695e1f567
BrvAlexis <brevieralexis@gmail.com>
2024-06-18 10:07:04 +0200

    category id

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -67,7 +67,7 @@ class WorkoutsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def workout_params
-      params.require(:workout).permit(:title, :description, :start_date, :duration, :city, :zip_code, :price, :host_id, :max_participants, :category_id, photos: [])
-      params.require(:workout).permit(:title, :description, :start_date, :duration, :city, :zip_code, :price, :max_participants, images: [])
+     
+      params.require(:workout).permit(:title, :description, :start_date, :duration, :city, :zip_code, :price, :max_participants, :category_id, images: [])
     end
 end
