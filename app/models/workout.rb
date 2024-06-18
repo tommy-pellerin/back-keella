476ba36f07a923e6c7b0a4445efd97d059483c21
BrvAlexis <brevieralexis@gmail.com>
2024-06-18 11:42:07 +0200

    workout_images ok

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -17,28 +17,28 @@ class Workout < ApplicationRecord
   validates :duration, presence: true, numericality: { greater_than_or_equal_to: 30 }
   validates :max_participants, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
-  # validate :start_date_must_be_at_least_4_hours_from_now
-  # validate :duration_must_be_multiple_of_30
+  validate :start_date_must_be_at_least_4_hours_from_now
+  validate :duration_must_be_multiple_of_30
 
-  # private
+  private
 
-  # def start_date_must_be_at_least_4_hours_from_now
-  #   if start_date.present? && start_date <= Time.now + 4.hours
-  #     errors.add(:start_date, "doit être au moins 4 heures après l'heure actuelle")
-  #   end
-  # end
-
-  # def duration_must_be_multiple_of_30
-  #   if duration.present? && duration % 30 != 0
-  #     errors.add(:duration, "doit être un multiple de 30 minutes")
-  #   end
-  # end
-
-  def image_url
-    if self.workout_images.attached?
-      workout_images.first.service_url
-    else
-      self.category.image_url
+  def start_date_must_be_at_least_4_hours_from_now
+    if start_date.present? && start_date <= Time.now + 4.hours
+      errors.add(:start_date, "doit être au moins 4 heures après l'heure actuelle")
     end
   end
+
+  def duration_must_be_multiple_of_30
+    if duration.present? && duration % 30 != 0
+      errors.add(:duration, "doit être un multiple de 30 minutes")
+    end
+  end
+
+  # def image_url
+  #   if self.workout_images.attached?
+  #     workout_images.first.service_url
+  #   else
+  #     self.category.image_url
+  #   end
+  # end
 end
