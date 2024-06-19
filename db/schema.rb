dee99c28e7280e9fe9a4b7bbafb141a032ffb1dd
WinnyDrancourt <winny.morel@orange.fr>
2024-06-19 11:51:38 +0200

    default status

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_18_060246) do
     t.bigint "workout_id", null: false
     t.integer "quantity"
     t.float "total"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_reservations_on_user_id"
