a6fc00cc7cb228c228505bf57afee49bacaae0b5
BrvAlexis <brevieralexis@gmail.com>
2024-06-28 09:51:54 +0200

    include username

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -7,16 +7,16 @@ class Rating < ApplicationRecord
   validates :user, uniqueness: { scope: [ :rateable_type, :rateable_id, :workout_id ], message: "Vous avez déjà noté cette ressource" }
   validate :workout_is_closed, on: :create
 
-  # def valid_rating_context?(user, rating, workout_id)
-  #   if rating.rateable_type == "Workout"
-  #     user.participated_workouts.exists?(id: rating.rateable_id)
-  #   elsif rating.rateable_type == "User"
-  #     workout = Workout.find(workout_id)
-  #     workout.participants.find(rating.rateable_id)
-  #   else
-  #     false
-  #   end
-  # end
+  def valid_rating_context?(user, rating, workout_id)
+  if rating.rateable_type == "Workout"
+  user.participated_workouts.exists?(id: rating.rateable_id)
+  elsif rating.rateable_type == "User"
+  workout = Workout.find(workout_id)
+  workout.participants.find(rating.rateable_id)
+  else
+  false
+  end
+  end
 
   private
 
