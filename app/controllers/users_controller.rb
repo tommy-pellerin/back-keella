0aa015747a0e76aabaebe0223082f1262d009718
tommy-pellerin <tommytp@hotmail.fr>
2024-06-29 12:50:16 +0200

    on going

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [ :show ]
+  before_action :set_user, only: [ :show, :destroy ]
 
   # GET /users
   def index
