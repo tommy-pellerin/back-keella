8d1c0c23c932bf2033adec26b6147447fb6350c1
tommy-pellerin <tommytp@hotmail.fr>
2024-06-29 14:01:40 +0200

    can delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,12 @@ class UsersController < ApplicationController
   # GET /users/:id
   def show
     if @user
-      user_json = @user.as_json(include: [ :reservations, :hosted_workouts, :participated_workouts, :ratings_received ])
+      user_json = @user.as_json(include: {
+        reservations: {},
+        hosted_workouts: { include: :reservations }, # Inclut les réservations pour chaque hosted_workout
+        participated_workouts: {},
+        ratings_received: {}
+      })
       user_json[:avatar] = @user.avatar.attached? ? url_for(@user.avatar) : nil
       user_json[:average_rating] = @user.ratings_received.any? ? @user.ratings_received.average(:rating).round(1) : 0
       render json: user_json
